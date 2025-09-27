3823ab0840e1278485d9257ea8aef17ad35c6922
TunisMan <131547316+TunisMan@users.noreply.github.com>
2025-09-27 10:18:04 +0300

    Implement all teleop controls and bindings

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,23 +4,45 @@
 
 package frc.robot;
 
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.drivetrain.SwerveSubsystem;
+import frc.robot.drivetrain.commands.TeleopSwerve;
 
 public class RobotContainer {
     private final SwerveSubsystem swerveSubsystem = new SwerveSubsystem();
     private final CommandXboxController driverController = new CommandXboxController(0);
+    private boolean fieldOriented = true;
+
+    private final TeleopSwerve teleopSwerve;
 
     public RobotContainer() {
+        teleopSwerve = new TeleopSwerve(
+            swerveSubsystem,
+            () -> -driverController.getLeftY(),
+            () -> -driverController.getLeftX(),
+            () -> -driverController.getRightX(),
+            () -> fieldOriented,
+            driverController.a()::getAsBoolean
+        );
+        swerveSubsystem.setDefaultCommand(teleopSwerve);
+
         configureBindings();
     }
 
     private void configureBindings() {
-        //TODO: Yarın eklenecek
+        driverController.start().onTrue(Commands.runOnce(() -> fieldOriented = !fieldOriented));
+
+        driverController.y().onTrue(Commands.runOnce(() -> 
+            teleopSwerve.setTargetRotation(
+                swerveSubsystem.getPose().getRotation().plus(Rotation2d.fromDegrees(180))
+            )
+        ));
     }
 
     public Command getAutonomousCommand() {
-        return null;
+        return null; //TODO: Otonom komutları
     }
 }
